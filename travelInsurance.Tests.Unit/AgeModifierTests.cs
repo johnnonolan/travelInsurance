2c52973568cb85f53ca800c511e8320f8da74d53
Sara Stephens <sara@sarastephens.com>
2009-11-26 20:43:16 +0000

    Sara's first attempt at the 3 way coding session

--- travelInsurance.Tests.Unit/AgeModifierTests.cs
+++ travelInsurance.Tests.Unit/AgeModifierTests.cs
@@ -5,21 +5,31 @@ namespace travelInsurance
     [TestFixture]
     public class AgeModifierTests
     {
-        [Test]
-        public void GetRating_18Years_ReturnRating()
+        [TestCase(18,1.2)]
+        [TestCase(19,1.0)]
+        [TestCase(46,1.2)]
+        public void GetRating_18Years_ReturnRating(int age, double expected)
         {
             AgeModifer ageModifier = new AgeModifer();
-            var modifier = ageModifier.GetRating(18);
-            Assert.AreEqual(1.2,modifier);
+            var modifier = ageModifier.GetRating(age);
+            Assert.AreEqual(expected,modifier);
 
         }
+
+
     }
 
     public class AgeModifer
     {
         public double GetRating(int age)
         {
+            if(age < 19)
             return 1.2;
+
+            if (age > 45 && age < 55)
+                return 1.2;
+
+            return 1.0;
         }
     }
 }

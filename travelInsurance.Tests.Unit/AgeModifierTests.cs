b07ed293b8e9a31fa48470c4b172f9c23c7de36b
Sara Stephens <sara@sarastephens.com>
2009-11-26 21:39:17 +0000

    Starting creating the SexModifier

--- travelInsurance.Tests.Unit/AgeModifierTests.cs
+++ travelInsurance.Tests.Unit/AgeModifierTests.cs
@@ -28,28 +28,12 @@ namespace travelInsurance
             var age = new AgeModifer();
             Assert.Throws<ArgumentException>(() => age.GetRating(-1));
         }
-    }
 
-    public class AgeModifer
-    {
-        public double GetRating(int age)
+        [Test]
+        public void GetRating_71_ThrowsDeclinedException()
         {
-            if (age < 0)
-                throw new ArgumentException("Age Must Be Greater Than or Equal to Zero.");
-
-            if (age.InRange(0, 18) || age.InRange(46, 55))
-                return 1.2;
-
-            if (age.InRange(19, 45))
-                return 1.0;
-
-            if (age.InRange(56, 65))
-                return 1.8;
-
-            if (age.InRange(66, 70))
-                return 2.0;
-
-            return -1;
+            var age = new AgeModifer();
+            Assert.Throws<DeclinedException>(() => age.GetRating(71));
         }
     }
 }

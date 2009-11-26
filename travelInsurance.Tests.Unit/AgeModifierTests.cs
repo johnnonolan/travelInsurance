4c204302c444c76569918bec3fdd5a3c48590fbc
Johnno Nolan <johnnonolan@yahoo.co.uk>
2009-11-26 20:28:20 +0000

    sad trombone

---
+++ travelInsurance.Tests.Unit/AgeModifierTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+namespace travelInsurance
+{
+    [TestFixture]
+    public class AgeModifierTests
+    {
+        [Test]
+        public void GetRating_18Years_ReturnRating()
+        {
+            AgeModifer ageModifier = new AgeModifer();
+            var modifier = ageModifier.GetRating(18);
+            Assert.AreEqual(1.2,modifier);
+
+        }
+    }
+
+    public class AgeModifer
+    {
+        public double GetRating(int age)
+        {
+            return 1.2;
+        }
+    }
+}
